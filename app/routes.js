48ddc78f4d7117c9c318ade081cb6a325bb14297
baarkerlounger <db@slothlife.xyz>
2022-10-19 15:47:34 +0100

    seed user

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const { User } = require('../models');
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/users', function (req, res) {
+    return User.findAll()
+    .then((users) => { 
+        res.send(users) 
+    })
+    .catch((err) => {
+      return res.send(err)
+    });
+  })
