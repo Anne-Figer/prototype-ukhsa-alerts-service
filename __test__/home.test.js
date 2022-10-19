84121ac8a2cbc7d0a53f913866bab2830497cf37
baarkerlounger <db@slothlife.xyz>
2022-10-19 09:57:38 +0100

    Add jest test

---
+++ __test__/home.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+const app = require('../server.js')
+
+describe("Test GET /", () => {
+  test("It should return OK", done => {
+    request(app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+
+  test("It should have the correct header", done => {
+    request(app)
+      .get("/")
+      .then(response => {
+        expect(response.text).toContain("UKHSA Temperature Alerts");
+        done();
+      });
+  });
+});
