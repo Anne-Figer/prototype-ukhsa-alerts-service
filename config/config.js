cc161f9a874ab287d92305b2c20909ec57cd9f58
baarkerlounger <db@slothlife.xyz>
2022-10-19 15:59:24 +0100

    Update test config

--- config/config.js
+++ config/config.js
@@ -8,11 +8,11 @@ module.exports = {
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DBUSER,
+    "password": process.env.DBPASS,
+    "database": process.env.DBNAME,
+    "host":  process.env.DBHOST,
+    "dialect": "postgres"
   },
   "production": {
     "username": process.env.DBUSER,
